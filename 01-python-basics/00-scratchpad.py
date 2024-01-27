42d29501a88768d25e8af8af3686041619c36efc
Walter Furrer <walterfurrer@proton.me>
2024-01-27 14:56:44 -0500

    Completed the Formatted Strings Excercise

--- 01-python-basics/00-scratchpad.py
+++ 01-python-basics/00-scratchpad.py
@@ -1,19 +1,58 @@
-user_info = {
-    "first_name": "Walter",
-    "last_name": "Furrer",
-    "age": 32,
-    "country": "USA",
-    "state": "NY",
-    "city": "Islip",
-    }
+# user_info = {
+#     "first_name": "Walter",
+#     "last_name": "Furrer",
+#     "age": 32,
+#     "country": "USA",
+#     "state": "NY",
+#     "city": "Islip",
+# }
 
-city_state = user_info["city"] + ", " + user_info["state"]
-full_name = user_info["first_name"] + " " + user_info["last_name"]
+# city_state = user_info["city"] + ", " + user_info["state"]
+# full_name = user_info["first_name"] + " " + user_info["last_name"]
 
-greeting = "Hi, my name is " + full_name + ". I am " + str(user_info["age"]) + " years old, and I live in " + city_state + "."
+# greeting = "Hi, my name is " + full_name + ". I am " + str(user_info["age"]) + " years old, and I live in " + city_state + "."
 
-print(greeting)
+# print(greeting)
 
-greeting2 = f"Hi, my name is {full_name}."
+# greeting2 = f"Hi, my name is {full_name}."
 
-print(greeting2)
+# print(greeting2)
+
+# a,b,c = 1,2,3
+# print(a)
+# print(b)
+# print(c)
+
+# iq = 100
+# user_age = iq / 5 # Entire line of code is known as a statement.
+# The value is known as the expression. AKA anything afer the =
+
+#augmented assignment operator
+# some_value = 5
+# # some_value = 5 + 2
+# some_value %= 2
+
+# print(some_value)
+
+# Type Conversion
+
+# print(str(100))
+# print(type(str(100)))
+# print(100)
+# print(type(100))
+
+# Escape Sequences
+
+# weather = 'It\'s sunny.'
+# print(weather)
+
+# weather2 = "\t \t It\'s \"kind of\" sunny. \n Hope you have a good day!"
+# print(weather2)
+
+#formatted strings
+
+# name = 'Walter'
+# age = 32
+
+# print('Hi ' + name + '! You are ' + str(age) + ' years old.')
+# print(f'Hi {name}! You are {age} years old.')
--- 01-python-basics/03-exercise-formatted-strings.py
+++ 01-python-basics/03-exercise-formatted-strings.py
@@ -10,3 +10,8 @@ print("Hello {name}, your balance is {amount}.".format(name="Cindy", amount=50))
 print("Hello {0}, your balance is {amount}.".format("Cindy", amount=50))
 
 # 2 How would you write this using f-string? (Scroll down for answer)
+
+name = "Cindy"
+amount = 50
+
+print(f"Hello {name}, your balance is {amount}. By the way, this is a formatted string!")
